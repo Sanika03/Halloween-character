d39c3548dc97e04c41fe463ba97bdcbb2e2bd2da
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-10-30 15:38:25 +0530

    took reference of html elements

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ next2Btn = document.querySelector("#next-2-btn");
 next3Btn = document.querySelector("#next-3-btn");
 next4Btn = document.querySelector("#next-4-btn");
 welcomeText = document.querySelector(".welcome-text");
-characterText = document.querySelector(".character-text")
+characterText = document.querySelector(".character-text");
+takeNameContainer = document.querySelector("#container-1");
+welcomeMssgContainer = document.querySelector("#container-2");
+personalitiesContainer = document.querySelector("#container-3");
+maleContainer = document.querySelector("#male-container")
+femaleContainer = document.querySelector("#female-container")
+characterContainer = document.querySelector("container-4")
 
 function showWelcomeMssg(){
     
