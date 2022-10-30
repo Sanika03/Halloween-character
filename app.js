c3f416427ed6f58f5b7de1d1a144ef0e55207ed9
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-10-30 15:20:27 +0530

    took reference of html elements

---
+++ app.js
@@ -0,0 +1,7 @@
+inputName = document.querySelector("#input");
+next1Btn = document.querySelector("#next-1-btn");
+next2Btn = document.querySelector("#next-2-btn");
+next3Btn = document.querySelector("#next-3-btn");
+next4Btn = document.querySelector("#next-4-btn");
+welcomeText = document.querySelector(".welcome-text");
+characterText = document.querySelector(".character-text")
