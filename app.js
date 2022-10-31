5adbb98bbc5af1a3208bc98a69885249dfcfa08a
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-10-31 13:55:07 +0530

    initial commit

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ welcomeText = document.querySelector(".welcome-text");
 characterText = document.querySelector(".character-text");
 takeNameContainer = document.querySelector(".container-1");
 welcomeMssgContainer = document.querySelector(".container-2");
-personalitiesContainer = document.querySelector(".container-3");
+chooseGenderContainer = document.querySelector(".container-3");
 maleContainer = document.querySelector(".male-container")
 femaleContainer = document.querySelector(".female-container")
 characterContainer = document.querySelector(".container-4")
+chooseForm = document.querySelector(".choose-form");
 
 function showWelcomeMssg(){
     takeNameContainer.style.display = "none";
@@ -18,4 +19,40 @@ function showWelcomeMssg(){
     welcomeText.innerText = "Hello " + `${inputName.value}` + "!\nLet's go find you a costume for Halloween 🎃";
 }
 
-next1Btn.addEventListener("click", showWelcomeMssg)
+function chooseGender(){
+    takeNameContainer.style.display = "none";
+    welcomeMssgContainer.style.display = "none";
+    chooseGenderContainer.style.display = "block";
+    // const formChoice = new FormData(chooseForm);
+    // const choices = ["male", "female"];
+    // let index = 0;
+    // for(let value of formChoice.values()){
+    //     console.log(value)
+    //     index++;
+    // }
+    
+    // if(formChoice.values() == "male"){
+    //     console.log("male");
+    // } else if(formChoice.values() == "female"){
+    //     console.log("female");
+    // }
+}
+
+function choosePersonality(){
+    takeNameContainer.style.display = "none";
+    chooseGenderContainer.style.display = "none";
+    console.log("bdshcb")
+    const formChoice = new FormData(chooseForm);
+    // let values = formChoice.values();
+    if(formChoice.values() === "male"){
+        maleContainer.style.display = "block";
+        console.log("male");
+    } else if(formChoice.values() === "female"){
+        console.log("female");
+        femaleContainer.style.display = "block";
+    }
+}
+
+next3Btn.addEventListener("click", choosePersonality);
+next2Btn.addEventListener("click", chooseGender);
+next1Btn.addEventListener("click", showWelcomeMssg);
